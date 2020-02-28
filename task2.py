eeed34bc56a56b30f3aebffe11604ca1e9ac9f75
igorpriadein <iga2456@gmail.com>
2020-02-28 18:17:00 +0600

    Task2 Part2

---
+++ task2.py
@@ -0,0 +1,11 @@
+students = int(input("Vvedite kolichestvo studentov"))
+apples = int(input("Vvedite kolichetvo yablok"))
+
+num1 = apples // students
+print("u kajdogo studenta budet", num1, "yablok")
+num2 = apples % students
+print("ostanetsa", num2, "yablok")
+# X = int(X)
+
+# print("ostanetsa", ostatok, "yablok")
+# print("u kajdogo studenta budet", eachone, "yablok")
